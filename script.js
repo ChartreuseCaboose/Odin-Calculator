9edb229f5b9d3f9a0e73910b6973da1c2897c8ab
ChartreuseCaboose <Jeffmosley42@gmail.com>
2023-04-14 18:14:25 -0500

    Make more scientific and beautiful

--- script.js
+++ script.js
@@ -9,17 +9,28 @@ function addToBuffer(input){
 function pushBuffer(){
     if (first == '') {
         first = buffer;
-        console.log('I pushed ', buffer);
         buffer = '';
         display.textContent = first;
     }
     else if (second == ''){
         second = buffer;
-        console.log('I pushed ', buffer);
         buffer = '';
         display.textContent = second;
     }
     else console.log('ERROR');
+}
+function backspace(){
+    let last = buffer.length -1;
+    buffer = buffer.slice(0, last);
+    display.textContent = buffer;
+}
+function clear(){
+    first = '';
+    second = '';
+    buffer = '';
+    operation = '';
+    result = '';
+    display.textContent = '';
 }
 //calculate calls the appropriate math function for readability's sake.
 function calculate(a, b, symbol){
@@ -33,27 +44,27 @@ function calculate(a, b, symbol){
         break;
         case '/': c = divide(a, b);
         break;
-        default: console.log('ERROR'); 
+        default: c = 'ERROR'; 
     }
     return c
 }
 function add(a, b){
-    let c = parseInt(a) + parseInt(b);
+    let c = Number(a) + Number(b);
     return c
 }
 function subtract(a, b){
-    let c = parseInt(a) - parseInt(b);
+    let c = Number(a) - Number(b);
     return c
 }
 function multiply(a, b){
-    let c = parseInt(a) * parseInt(b);
+    let c = Number(a) * Number(b);
     return c
 }
 function divide(a, b){
     if (b == 0){
         return 'ERROR'
     }
-    let c = parseInt(a) / parseInt(b);
+    let c = Number(a) / Number(b);
     return c
 }
 //execute() and operate() are the workhorses. execute() is the simple case, called by the '=' key or when moving on to a second operation with both storage variables (first, second) full. After completion it clears all storage variables.
@@ -66,7 +77,6 @@ function execute(){
     second = '';
     operation = '';
     buffer = '';
-    console.log(result);
     display.textContent = result;
     return
 }
@@ -76,12 +86,12 @@ function operate(input){
     if (first != '' && second != ''){
         execute();
         first = result;
-        console.log('I solved the equation!')
     }
     else if (first == '' && result != ''){
         first = result;
     }
     operation = input;
+    display.textContent = operation;
     return
 }
 const nums = document.querySelectorAll('.num');
@@ -100,4 +110,12 @@ const equals = document.querySelector('.equals');
 equals.addEventListener('click', function(){
     execute();
     second = '';
+});
+const clearKey = document.querySelector('.clear');
+clearKey.addEventListener('click', function(){
+    clear();
+});
+const backspaceKey = document.querySelector('.backspace');
+backspaceKey.addEventListener('click', function(){
+    backspace();
 });
